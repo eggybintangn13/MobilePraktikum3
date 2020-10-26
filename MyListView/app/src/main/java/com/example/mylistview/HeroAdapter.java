526d41366f394b4e95141953907feec5a611f91a
eggybintangn13 <eggybintangn@gmail.com>
2020-10-26 16:58:26 +0700

    Praktikum 3 ListView

--- MyListView/app/src/main/java/com/example/mylistview/HeroAdapter.java
+++ MyListView/app/src/main/java/com/example/mylistview/HeroAdapter.java
@@ -55,13 +55,13 @@ public class HeroAdapter extends BaseAdapter {
         private TextView txtDescription;
         private ImageView imgPhoto;
 
-        ViewHolder(View view){
+        ViewHolder(View view) {
             txtName = view.findViewById(R.id.txt_name);
             txtDescription = view.findViewById(R.id.txt_description);
             imgPhoto = view.findViewById(R.id.img_photo);
         }
 
-        void bind(Hero hero){
+        void bind(Hero hero) {
             txtName.setText(hero.getName());
             txtDescription.setText(hero.getDescription());
             imgPhoto.setImageResource(hero.getPhoto());
